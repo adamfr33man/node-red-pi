4e30058c0b77bffe8113269154a6647d71cf4fff
adamfr33mn <shavenyaknzgmail.com>
2015-07-18 23:39:54 +1200

    A bit of a tidy up

--- install.sh
+++ install.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
 echo 'Updating dependancies...'
-#apt-get update
+apt-get update
 
 echo 'Installing dependancies'
-#apt-get install -y git-core git scons build-essential scons libpcre++-dev libboost-dev libboost-program-options-dev libboost-thread-dev libboost-filesystem-dev
+apt-get install -y git-core git scons build-essential scons libpcre++-dev libboost-dev libboost-program-options-dev libboost-thread-dev libboost-filesystem-dev
 
 # Get the latest version of Node
 echo 'Getting the most recent version of Node'
-#wget http://nodejs.org/dist/v0.10.28/node-v0.10.28.tar.gz
-#tar xvf node-v0.10.28.tar.gz
-#cd node-v0.10.28
-# Installing Node
-#echo 'Installing Node, this will take a loooong time, at least an hour'
-#./configure
-#make
-#make install
 PACKAGE=node_latest_armhf.deb
 echo 'Downloading Node ...'
 wget http://node-arm.herokuapp.com/$PACKAGE
@@ -28,10 +20,9 @@ node -v
 echo 'npm version:'
 npm -v
 
-
 # Install Node-red, Mosca and plugins for Node-red
-echo 'Installing Node-Red Dependancies' 
-npm install -g mosca bunyan 
+echo 'Installing Node-Red Dependancies'
+npm install -g mosca bunyan
 # Isntalling Node-red
 echo 'Installing Node-Red'
 npm install -g node-red
